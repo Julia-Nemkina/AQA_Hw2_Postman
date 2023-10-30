e44162db5f2ea6215bb873be163c5a16609d9ef0
Julia Nemkina <nemkina.julia@mail.ru>
2023-10-30 20:48:56 +0300

    Postman without error

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -19,6 +19,6 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hi"));
+                .body("data", equalTo("Hello")); // changed
     }
 }
